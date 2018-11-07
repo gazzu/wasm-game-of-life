abdbc5d08853abb44f30f7367cdd374d801df649
Marco Gazzuolo <marco.gazzuolo@INTERLOGICA.LAN>
2018-11-07 16:30:26 +0100

    running on canvas

--- pkg/wasm_game_of_life.js
+++ pkg/wasm_game_of_life.js
@@ -1,6 +1,10 @@
 /* tslint:disable */
 import * as wasm from './wasm_game_of_life_bg';
 
+/**
+*/
+export const Cell = Object.freeze({ Dead:0,Alive:1, });
+
 let cachedTextDecoder = new TextDecoder('utf-8');
 
 let cachegetUint8Memory = null;
@@ -15,34 +19,88 @@ function getStringFromWasm(ptr, len) {
     return cachedTextDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
 }
 
-export function __wbg_alert_d3b6e8db27c82dfa(arg0, arg1) {
-    let varg0 = getStringFromWasm(arg0, arg1);
-    alert(varg0);
+let cachedGlobalArgumentPtr = null;
+function globalArgumentPtr() {
+    if (cachedGlobalArgumentPtr === null) {
+        cachedGlobalArgumentPtr = wasm.__wbindgen_global_argument_ptr();
+    }
+    return cachedGlobalArgumentPtr;
 }
 
-let cachedTextEncoder = new TextEncoder('utf-8');
+let cachegetUint32Memory = null;
+function getUint32Memory() {
+    if (cachegetUint32Memory === null || cachegetUint32Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint32Memory = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachegetUint32Memory;
+}
 
-function passStringToWasm(arg) {
+function freeUniverse(ptr) {
 
-    const buf = cachedTextEncoder.encode(arg);
-    const ptr = wasm.__wbindgen_malloc(buf.length);
-    getUint8Memory().set(buf, ptr);
-    return [ptr, buf.length];
+    wasm.__wbg_universe_free(ptr);
 }
 /**
-* @param {string} arg0
-* @returns {void}
 */
-export function greet(arg0) {
-    const [ptr0, len0] = passStringToWasm(arg0);
-    try {
-        return wasm.greet(ptr0, len0);
+export class Universe {
 
-    } finally {
-        wasm.__wbindgen_free(ptr0, len0 * 1);
+    static __wrap(ptr) {
+        const obj = Object.create(Universe.prototype);
+        obj.ptr = ptr;
 
+        return obj;
     }
 
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+        freeUniverse(ptr);
+    }
+
+    /**
+    * @returns {void}
+    */
+    tick() {
+        return wasm.universe_tick(this.ptr);
+    }
+    /**
+    * @returns {Universe}
+    */
+    static new() {
+        return Universe.__wrap(wasm.universe_new());
+    }
+    /**
+    * @returns {string}
+    */
+    render() {
+        const retptr = globalArgumentPtr();
+        wasm.universe_render(retptr, this.ptr);
+        const mem = getUint32Memory();
+        const rustptr = mem[retptr / 4];
+        const rustlen = mem[retptr / 4 + 1];
+
+        const realRet = getStringFromWasm(rustptr, rustlen).slice();
+        wasm.__wbindgen_free(rustptr, rustlen * 1);
+        return realRet;
+
+    }
+    /**
+    * @returns {number}
+    */
+    width() {
+        return wasm.universe_width(this.ptr);
+    }
+    /**
+    * @returns {number}
+    */
+    height() {
+        return wasm.universe_height(this.ptr);
+    }
+    /**
+    * @returns {number}
+    */
+    cells() {
+        return wasm.universe_cells(this.ptr);
+    }
 }
 
 export function __wbindgen_throw(ptr, len) {
