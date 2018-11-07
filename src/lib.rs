257ab3b1c33d82085c89f687e3e7907e9ab6093c
Marco Gazzuolo <marco.gazzuolo@INTERLOGICA.LAN>
2018-11-07 09:32:14 +0100

    hello name

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: String) {
+	let mut start: String = "Hello ".to_owned();
+	let end: String = ", wasm-game-of-life!".to_owned();
+	start.push_str(&name);
+	start.push_str(&end);
+    alert(&start);
+}
